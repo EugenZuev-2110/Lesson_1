890c5cac0af4c870f402548065eb66ace625f0e5
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-08-30 15:55:03 +0300

    Constructor added

--- src/Lesson_1/ImmuClass.java
+++ src/Lesson_1/ImmuClass.java
@@ -1,6 +1,13 @@
 package Lesson_1;
 
-public class ImmuClass {
+import java.util.Date;
+
+public final class ImmuClass {
 	 private final String name;
-	    private final Date birthDate;
+	 private final Date birthDate;
+	 
+	 public ImmuClass(String name, Date birthDate) {
+	        this.name = name;
+	        this.birthDate = birthDate;
+	    }
 }
